00200609ec1979f524dfde673759a8fee1dd4a55
Walker <walker.m.oliver@gmail.com>
2023-01-19 12:58:31 -0500

    Add logic to check for a match

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,12 @@ function Card(props) {
   const { side, name, flipCard, id } = props;
 
   return (
-    <div onClick={flipCard} className={`Card Card-${side}`} id={id}>
+    <div
+      onClick={flipCard}
+      className={`Card Card-${side}`}
+      id={id}
+      data-name={name}
+    >
       {side === 'front' ? name : '?'}
     </div>
   );
--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ScoreBoard from './ScoreBoard';
 import CardContainer from './CardContainer';
 import generateDeck from './helpers/generateDeck';
@@ -7,11 +7,14 @@ import './styles/Game.css';
 function Game() {
   const initialDeck = generateDeck();
   const [cards, setCards] = useState(initialDeck);
+  const [flippedCards, setFlippedCards] = useState([]);
 
   // on mount:
   // call function to randomize card order & setCards
 
   const flipCard = (e) => {
+    if (e.target.classList.contains('Card-front')) return;
+
     setCards(
       cards.map((c) =>
         c.id === e.target.id
@@ -19,7 +22,33 @@ function Game() {
           : c
       )
     );
+    setFlippedCards([...flippedCards, e.target.dataset.name]);
   };
+
+  // Check for a match after cards are flipped
+  useEffect(() => {
+    const checkForMatch = () => {
+      if (flippedCards.length !== 2) return;
+
+      // if different, flip back
+      if (flippedCards[0] !== flippedCards[1]) {
+        setTimeout(
+          () =>
+            setCards(
+              cards.map((c) =>
+                c.name === flippedCards[0] || c.name === flippedCards[1]
+                  ? { ...c, side: 'back' }
+                  : c
+              )
+            ),
+          1000
+        );
+      }
+      setFlippedCards([]);
+    };
+
+    checkForMatch();
+  }, [cards, flippedCards]);
 
   return (
     <div className="Game">
