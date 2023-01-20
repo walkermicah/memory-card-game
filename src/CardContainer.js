038c749a553e73edaea52698df971d9bccbb6713
Walker <walker.m.oliver@gmail.com>
2023-01-20 10:27:21 -0500

    Add conditional rendering logic for gameOver

--- src/CardContainer.js
+++ src/CardContainer.js
@@ -2,17 +2,18 @@ import React from 'react';
 import Card from './Card';
 import './styles/CardContainer.css';
 
-function CardContainer(props) {
+function CardContainer({ cards, flipCard, gameOver }) {
   return (
-    <div className="CardContainer">
-      {props.cards.map((c) => (
+    <div className="CardContainer" style={{ opacity: `${gameOver ? 0.4 : 1}` }}>
+      {cards.map((c) => (
         <Card
           key={c.id}
           name={c.name}
           side={c.side}
           id={c.id}
+          imgUrl={c.imgUrl}
           disabled={c.disabled}
-          flipCard={props.flipCard}
+          flipCard={flipCard}
         />
       ))}
     </div>
