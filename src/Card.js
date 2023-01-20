8dd2a36b738e11a2456f455ba2a47de3b84a6dcf
Walker <walker.m.oliver@gmail.com>
2023-01-20 10:26:35 -0500

    Add image URLs

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles/Card.css';
 
 function Card(props) {
-  const { side, name, flipCard, id, disabled } = props;
+  const { side, name, flipCard, id, imgUrl, disabled } = props;
 
   return (
     <div
@@ -12,7 +12,7 @@ function Card(props) {
       data-name={name}
       disabled={disabled}
     >
-      {side === 'front' ? name : '?'}
+      {side === 'front' ? <img src={imgUrl} alt={name} /> : '?'}
     </div>
   );
 }
--- src/helpers/generateRandomDeck.js
+++ src/helpers/generateRandomDeck.js
@@ -1,31 +1,135 @@
 import uniqid from 'uniqid';
+import daVinci from '../img/daVinci.png';
+import warhol from '../img/warhol.png';
+import dali from '../img/dali.png';
+import pollock from '../img/pollock.png';
+import picasso from '../img/picasso.png';
+import kahlo from '../img/kahlo.png';
+import basquiat from '../img/basquiat.png';
+import kandinsky from '../img/kandinsky.png';
+import mondrian from '../img/mondrian.png';
+import magritte from '../img/magritte.png';
 
 const initialDeck = [
-  { name: '1', side: 'back', id: uniqid(), disabled: false },
-  { name: '1', side: 'back', id: uniqid(), disabled: false },
-  { name: '2', side: 'back', id: uniqid(), disabled: false },
-  { name: '2', side: 'back', id: uniqid(), disabled: false },
-  { name: '3', side: 'back', id: uniqid(), disabled: false },
-  { name: '3', side: 'back', id: uniqid(), disabled: false },
-  { name: '4', side: 'back', id: uniqid(), disabled: false },
-  { name: '4', side: 'back', id: uniqid(), disabled: false },
-  { name: '5', side: 'back', id: uniqid(), disabled: false },
-  { name: '5', side: 'back', id: uniqid(), disabled: false },
-  { name: '6', side: 'back', id: uniqid(), disabled: false },
-  { name: '6', side: 'back', id: uniqid(), disabled: false },
-  { name: '7', side: 'back', id: uniqid(), disabled: false },
-  { name: '7', side: 'back', id: uniqid(), disabled: false },
-  { name: '8', side: 'back', id: uniqid(), disabled: false },
-  { name: '8', side: 'back', id: uniqid(), disabled: false },
-  { name: '9', side: 'back', id: uniqid(), disabled: false },
-  { name: '9', side: 'back', id: uniqid(), disabled: false },
-  { name: '10', side: 'back', id: uniqid(), disabled: false },
-  { name: '10', side: 'back', id: uniqid(), disabled: false },
+  {
+    name: 'daVinci',
+    side: 'back',
+    id: uniqid(),
+    disabled: false,
+    imgUrl: daVinci,
+  },
+  {
+    name: 'daVinci',
+    side: 'back',
+    id: uniqid(),
+    disabled: false,
+    imgUrl: daVinci,
+  },
+  {
+    name: 'Warhol',
+    side: 'back',
+    id: uniqid(),
+    disabled: false,
+    imgUrl: warhol,
+  },
+  {
+    name: 'Warhol',
+    side: 'back',
+    id: uniqid(),
+    disabled: false,
+    imgUrl: warhol,
+  },
+  { name: 'Dali', side: 'back', id: uniqid(), disabled: false, imgUrl: dali },
+  { name: 'Dali', side: 'back', id: uniqid(), disabled: false, imgUrl: dali },
+  {
+    name: 'Pollock',
+    side: 'back',
+    id: uniqid(),
+    disabled: false,
+    imgUrl: pollock,
+  },
+  {
+    name: 'Pollock',
+    side: 'back',
+    id: uniqid(),
+    disabled: false,
+    imgUrl: pollock,
+  },
+  {
+    name: 'Picasso',
+    side: 'back',
+    id: uniqid(),
+    disabled: false,
+    imgUrl: picasso,
+  },
+  {
+    name: 'Picasso',
+    side: 'back',
+    id: uniqid(),
+    disabled: false,
+    imgUrl: picasso,
+  },
+  { name: 'Kahlo', side: 'back', id: uniqid(), disabled: false, imgUrl: kahlo },
+  { name: 'Kahlo', side: 'back', id: uniqid(), disabled: false, imgUrl: kahlo },
+  {
+    name: 'Basquiat',
+    side: 'back',
+    id: uniqid(),
+    disabled: false,
+    imgUrl: basquiat,
+  },
+  {
+    name: 'Basquiat',
+    side: 'back',
+    id: uniqid(),
+    disabled: false,
+    imgUrl: basquiat,
+  },
+  {
+    name: 'Kandinsky',
+    side: 'back',
+    id: uniqid(),
+    disabled: false,
+    imgUrl: kandinsky,
+  },
+  {
+    name: 'Kandinsky',
+    side: 'back',
+    id: uniqid(),
+    disabled: false,
+    imgUrl: kandinsky,
+  },
+  {
+    name: 'Mondrian',
+    side: 'back',
+    id: uniqid(),
+    disabled: false,
+    imgUrl: mondrian,
+  },
+  {
+    name: 'Mondrian',
+    side: 'back',
+    id: uniqid(),
+    disabled: false,
+    imgUrl: mondrian,
+  },
+  {
+    name: 'Magritte',
+    side: 'back',
+    id: uniqid(),
+    disabled: false,
+    imgUrl: magritte,
+  },
+  {
+    name: 'Magritte',
+    side: 'back',
+    id: uniqid(),
+    disabled: false,
+    imgUrl: magritte,
+  },
 ];
 
-function generateRandomDeck() {
+export default function generateRandomDeck() {
   const randomDeck = initialDeck.sort(() => 0.5 - Math.random());
   return randomDeck;
 }
-
-export default generateRandomDeck;
